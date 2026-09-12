d97f88c5147ad95daaaabe243dcfc64ce0d7aeef
winipe76 <winipe76@gmail.com>
2026-09-12 13:35:12 +0900

    Add QoQ EPS snapshot fields

---
+++ drizzle/0009_eps_qoq_score.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `fundamental_snapshots` ADD `previous_quarter_eps` real;
+--> statement-breakpoint
+ALTER TABLE `fundamental_snapshots` ADD `eps_qoq_pct` real;
+--> statement-breakpoint
+ALTER TABLE `fundamental_snapshots` ADD `eps_qoq_status` text;
+
